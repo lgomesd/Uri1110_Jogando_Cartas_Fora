0d15c95af326216a124d8449b83dd8b896175d0d
lgome <lgome@LAPTOP-LRE3EK8M>
2019-09-21 09:40:54 -0300

    Merge origin/master
    Conflicts:
    	CifraDeCesar/src/main/java/br/lgomesd/cifradecesar/CifraDeCesar.java

--- src/main/java/br/lgomesd/uri1110_jogando_cartas_fora/Main.java
+++ src/main/java/br/lgomesd/uri1110_jogando_cartas_fora/Main.java
@@ -19,35 +19,43 @@ public class Main {
     public static void main(String[] args) {
         
         Random rng = new Random();
-        Integer cartas = rng.nextInt(10);
+        Integer cartas = rng.nextInt(10)+1;
         Integer vetorCartas[] = new Integer[cartas];
         Integer vetorDescarte[] = new Integer[cartas-1];
         Integer contador = 0;
-        Integer uPosicao = cartas;
+        Integer uPosicao = cartas-1;
         Integer aux;
         System.out.println(cartas);
         
-        for(int i = 0;i<vetorCartas.length;i++)
+        //System.out.print("Vetor de cartas: ");
+        for(int i = 0;i<vetorCartas.length;i++){
             vetorCartas[i] = rng.nextInt(10);
-        
+            //System.out.print(vetorCartas[i] + ", ");
+        }
+        System.out.println("\n");
         for(int i = vetorCartas.length-1;i > 0;i--){
             vetorDescarte[contador] = vetorCartas[i];
-            vetorCartas[i]=null;
+            //vetorCartas[i]=null;
+            aux = vetorCartas[i-1];
             for(int x = uPosicao;x>1;x--){
-                aux = vetorCartas[x-1];
+                
                 vetorCartas[x-1] = vetorCartas[x-2];
-                vetorCartas[0] = vetorCartas[aux];
-                System.out.println(vetorCartas[i]);
+                
+                //System.out.println(vetorCartas[i]);
             }
-            System.out.println(vetorCartas[i]);
+            vetorCartas[0] = vetorCartas[aux];
+            //System.out.println(vetorCartas[i]);
             uPosicao--;
             contador++;
             
                
         }
-//        for(int i = 0;i<vetorDescarte.length;i++)
-//            System.out.println(vetorDescarte[i]);
-        
+        System.out.print("Discarded cards: ");
+        for(int i = 0;i<vetorDescarte.length;i++){
+            System.out.print(vetorDescarte[i] + ", ");
+        }
+        System.out.println("\n");
+        System.out.println("Remaining card: " + vetorCartas[0]);
     }
     
 }
