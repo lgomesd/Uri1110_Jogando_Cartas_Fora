35a08481dcedfd4b94939e8397de9b226bf8e37e
lgome <lgome@LAPTOP-LRE3EK8M>
2019-09-21 12:08:38 -0300

    Merge origin/master
    Conflicts:
    	CifraDeCesar/src/main/java/br/lgomesd/cifradecesar/CifraDeCesar.java

--- src/main/java/br/lgomesd/uri1110_jogando_cartas_fora/Main.java
+++ src/main/java/br/lgomesd/uri1110_jogando_cartas_fora/Main.java
@@ -5,7 +5,8 @@
  */
 package br.lgomesd.uri1110_jogando_cartas_fora;
 
-import java.util.Random;
+import java.util.Scanner;
+
 
 /**
  *
@@ -18,40 +19,51 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        Random rng = new Random();
-        Integer cartas = rng.nextInt(10)+1;
-        Integer vetorCartas[] = new Integer[cartas];
-        Integer vetorDescarte[] = new Integer[cartas-1];
-        Integer contador = 0;
-        Integer uPosicao = cartas-1;
-        Integer aux;
-        System.out.println(cartas);
+        Scanner ler = new Scanner(System.in);
+        Integer cartas;
         
-        System.out.print("Vetor de cartas: ");
-        for(int i = 0;i<vetorCartas.length;i++){
-            vetorCartas[i] = rng.nextInt(10);
-            System.out.print(vetorCartas[i] + ", ");
-        }
-        System.out.println("\n");
-        for(int i = vetorCartas.length-1;i > 0;i--){
-            vetorDescarte[contador] = vetorCartas[i];
-            //vetorCartas[i]=null;
-            aux = vetorCartas[i-1];
-            for(int x = uPosicao;x>1;x--){                
-                vetorCartas[x-1] = vetorCartas[x-2];                
-                //System.out.println(vetorCartas[i]);
+        
+        do{
+            cartas = ler.nextInt();
+            if(cartas>0){
+            Integer vetorCartas[] = new Integer[cartas];
+            Integer vetorDescarte[] = new Integer[cartas-1];
+            Integer uPosicao = cartas-1;
+        
+            Integer contador = 0;
+
+            Integer aux;
+            //System.out.println(cartas);
+
+            //System.out.print("Vetor de cartas: ");
+            for(int i = cartas-1;i>=0;i--){
+                    vetorCartas[i] = contador+1;
+                    contador++;
+               // System.out.print(vetorCartas[i] + ", ");
             }
-            vetorCartas[0] = vetorCartas[aux];
-            //System.out.println(vetorCartas[i]);
-            uPosicao--;
-            contador++;                        
-        }
-        System.out.print("Discarded cards: ");
-        for(int i = 0;i<vetorDescarte.length;i++){
-            System.out.print(vetorDescarte[i] + ", ");
-        }
-        System.out.println("\n");
-        System.out.println("Remaining card: " + vetorCartas[0]);
+            //System.out.println("\n");
+            contador = 0;
+            for(int i = vetorCartas.length-1;i > 0;i--){
+                vetorDescarte[contador] = vetorCartas[i];
+                aux = vetorCartas[i-1];
+                for(int x = uPosicao;x>1;x--){                
+                    vetorCartas[x-1] = vetorCartas[x-2];                
+                }
+                vetorCartas[0] = aux;
+                uPosicao--;
+                contador++;                        
+            }
+            System.out.print("Discarded cards: ");
+            for(int i = 0;i<vetorDescarte.length;i++){
+                if(i<vetorDescarte.length-1)
+                    System.out.print(vetorDescarte[i] + ", ");
+                else
+                    System.out.print(vetorDescarte[i]);              
+            }
+            System.out.print("\n");
+            System.out.print("Remaining card: " + vetorCartas[0]);
+            }
+        }while(cartas != 0);
     }
     
 }
